f00fe8614eba757895f375f742910321617b8038
root <root@DESKTOP-V5NIGCN.localdomain>
2024-12-18 04:03:13 -0300

    feat: ensure SignUpController calls CreateAccount with correct values

--- src/presetation/controllers/signup.ts
+++ src/presetation/controllers/signup.ts
@@ -1,12 +1,15 @@
+import { CreateAccount } from './../../domain/usecases/create-account'
 import { HttpResponse, HttpRequest, Controller, EmailValidator } from '../protocols'
 import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly createAccount: CreateAccount
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, createAccount: CreateAccount) {
     this.emailValidator = emailValidator
+    this.createAccount = createAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -25,6 +28,11 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+      this.createAccount.create({
+        name,
+        email,
+        password
+      })
     } catch (error) {
       return serverError()
     }
